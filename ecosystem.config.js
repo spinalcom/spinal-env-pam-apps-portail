08a29b41792b6a0a90e20f45cda22f4ac67c9701
SSRayane <r.serir@spinalcom.com>
2024-10-22 18:07:40 +0200

    ecosystem and clean

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,21 @@
-const port = 9070;
-
+require('dotenv').config();
+const port = process.env.PORT;
+const protocol = process.env.PROTOCOL || 'http';
+const ssl_cert = process.env.SSL_CERT || '';
+const ssl_key = process.env.SSL_KEY || '';
+const args = `dist/ -p ${port} `;
+if(protocol ==='https'){
+  args+= `--ssl --cert ${ssl_cert} --key ${ssl_key}`;
+}
 module.exports = {
   apps: [
     {
       name: `portail-applicatif-pam-${port}`,
       script: "node_modules/.bin/http-server",
       cwd: ".",
-      // args: `serve -p ${port} index.html micro-apps/*/index.html --dist-dir dist/`, // parcel
-      args: `dist/ -p ${port}`, // http-server
+      args: args
     },
   ],
 };
+
+
