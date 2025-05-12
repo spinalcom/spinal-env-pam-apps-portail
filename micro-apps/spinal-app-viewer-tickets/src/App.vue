374bc8c1e4ac5aed037674dc7523edd94b860aad
Aladdin <a.nasri@spinalcom.com>
2025-05-12 16:46:42 +0200

    Bos config Ticket

--- micro-apps/spinal-app-viewer-tickets/src/App.vue
+++ micro-apps/spinal-app-viewer-tickets/src/App.vue
@@ -464,23 +464,51 @@ class App extends Vue {
     switch (item?.type) {
       case undefined:
         const buildingId = localStorage.getItem("idBuilding");
+        if (buildingId) {
+          const playload = {
+            config,
+            item: { buildingId, type: "building" },
+          };
 
-        const promises = [
-          this.$store.dispatch(ActionTypes.GET_BUILDING_BY_ID, { buildingId })
-        ];
+          const promises = [
+            this.$store.dispatch(ActionTypes.GET_BUILDING_BY_ID, { buildingId }),
+          ];
 
-        const [building] = await Promise.all(promises);
+          const [building, items] = await Promise.all(promises);
 
-        return [
-          {
-            name: building.name,
-            staticId: building.id,
-            categories: [],
-            color: "#35CAE5",
-            dynamicId: 0,
-            type: "building",
-          },
-        ];
+          const realBuilding = await this.$store.dispatch(
+            ActionTypes.GET_BOS_BUILDING,
+            { buildingId }
+          )
+          return [
+            {
+              name: realBuilding.name,
+              staticId: building.id,
+              categories: [],
+              color: realBuilding.color,
+              dynamicId: realBuilding.dynamicId,
+              type: 'building',
+            },
+          ];
+        } else {
+          const building = await this.$store.dispatch(
+            ActionTypes.GET_BOS_BUILDING,
+            {
+              buildingId: null,
+            }
+          );
+          console.log(building);
+          return [
+            {
+              name: building.name,
+              staticId: building.id,
+              categories: [],
+              color: '#35CAE5',
+              dynamicId: building.dynamicId,
+              type: 'building',
+            },
+          ];
+        }
 
       case "building":
 
