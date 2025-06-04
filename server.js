253f1d081e106be5ae1ea94457e8553dfa4a5f6a
mbt-tech <mbaradji221@outlook.fr>
2025-06-04 20:19:33 +0200

    modify server.js to exchange ref by token

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const dotenv = require("dotenv");
 const { checkTokenValidity } = require("./serverUtils");
 const https = require("https");
 const fs = require("fs");
+const { default: axios } = require('axios');
 
 
 dotenv.config({ path: path.resolve(__dirname, ".env") });
@@ -14,26 +15,40 @@ const port = process.env.PORT || 1234;
 const vue_dir = path.resolve(__dirname, './dist');
 const app = express();
 
+let spinal_api_url = process.env.SPINAL_API_URL.replace(/\/$/, ""); // Ensure no trailing slash
+
 app.use(cors("*"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(vue_dir));
 app.use(cookieParser());
 
 
-app.use(express.static(vue_dir));
+app.get("/", (req, res) => {
+    const queries = req.query;
+    if (queries.ref) {
+        return res.redirect("/login?ref=" + queries.ref);
+    }
+});
 
-app.use("/login", async (req, res) => {
-    console.log("cookies on login request", req.cookies);
-    const token = req.cookies.token;
-    let spinal_api_url = process.env.SPINAL_API_URL;
+app.get("/login", async (req, res) => {
 
-    if (token) {
-        const { error } = await checkTokenValidity(spinal_api_url, token);
-        if (!error) return res.redirect("/home");
+    let token = req.cookies.token;
+    if (!token && req.query.ref) token = await getTokentoKenByRef(req.query.ref);
+
+
+    if (!token) {
+        return res.redirect(spinal_api_url + "/login");
     }
 
-    if (!spinal_api_url.endsWith("/")) spinal_api_url += "/";
-    return res.redirect(spinal_api_url + "login");
+
+    // Check if the token is valid
+    const { error } = await checkTokenValidity(spinal_api_url, token);
+    if (error) return res.redirect(spinal_api_url + "/login");
+
+
+    res.cookie("token", token);
+    return res.sendFile(path.resolve(vue_dir, 'index.html'));
 });
 
 app.use("/*", (req, res) => {
@@ -51,4 +66,16 @@ if (process.env.PROTOCOL === "http") {
     };
 
     https.createServer(sslOptions, app).listen(port, () => console.log(`app listening at https://localhost:${port} ....`));
+}
+
+
+
+
+function getTokentoKenByRef(ref) {
+    return axios.get(`${spinal_api_url}/getTokenByRef/${ref}`).then((result) => {
+        if (result.status != 200) return null;
+        return result.data.token;
+    }).catch(err => {
+        return null;
+    });
 }
